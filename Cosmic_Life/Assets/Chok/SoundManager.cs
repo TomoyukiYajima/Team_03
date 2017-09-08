792bebadeb7b1536ab9e632b72b01e2655f950ba
Jie-PC\Jie <jiex312@gmail.com>
2017-09-08 11:19:27 +0900

    SoundManager追加

--- Cosmic_Life/Assets/Chok/SoundManager.cs
+++ Cosmic_Life/Assets/Chok/SoundManager.cs
@@ -1,18 +1,12 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public enum SceneType
-{
-    LogoScene,
-    Title,
-    sample,
-}
-
-public class SceneManager : MonoBehaviour {
+public class SoundManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		
 	}
 	
 	// Update is called once per frame
